b8a9d47e5cad9ded48be0a81c9862a9b1f64d762
Changmao Li <changmaoli@ChangmaodeMBP.attlocal.net>
2019-02-27 06:21:10 -0500

    add pca function

--- src/preprocess.py
+++ src/preprocess.py
@@ -41,6 +41,18 @@ class Preprocessor:
         else:
             return value
 
+    def pca(self, X=np.array([]), no_dims=50):
+        """
+            Runs PCA on the NxD array X in order to reduce its dimensionality to
+            no_dims dimensions.
+        """
+        print("Preprocessing the data using PCA...")
+        (n, d) = X.shape
+        X = X - np.tile(np.mean(X, 0), (n, 1))
+        (l, M) = np.linalg.eig(np.dot(X.T, X))
+        Y = np.dot(X, M[:, 0:no_dims])
+        return Y
+
     def get_data_file(self):
         data_expression_median = "1jV89yYOUBWnPPC4oOhJGo_MaIwPQ2Nk0"
         data_CNA = "1ac045VSEWOxgkSdZREdW3z5KNRpoObCt"
