d81bc443f62dba86f94b7fc990b924b0cf18090b
Changmao Li <changmaoli@ChangmaodeMBP.attlocal.net>
2019-03-02 10:49:56 -0500

    Add mlp baseline

--- src/Baseline.py
+++ src/Baseline.py
@@ -1,20 +1,18 @@
-from preprocess import Preprocessor
+from src.preprocess import Preprocessor
 import numpy as np
 from random import randint
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#preprocessing data
-preprocessor = Preprocessor()
-
-clinical_X = preprocessor.clinical_X
-clinical_Y = preprocessor.clinical_Y
-
-genomic_X = preprocessor.genomic_X
-genomic_Y = preprocessor.genomic_Y
-
-
-#devide data set into 8:1:1 as train,validate,test set
+#Using 7 baseline methods to predict and compute results' AUC.
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import  MLPClassifier
 
 def devide(X,Y,kFolds):
     devided = [[],[],[],[],[],[]]
@@ -33,25 +31,12 @@ def devide(X,Y,kFolds):
         i += 1
     return [ _ for _ in devided ]
 
-Ctr_X,Ctr_Y,Cval_X,Cval_Y,Ct_X,Ct_Y = devide(clinical_X,clinical_Y,10)
-Gtr_X,Gtr_Y,Gval_X,Gval_Y,Gt_X,Gt_Y = devide(genomic_X,genomic_Y,10)
-
-
-
-#Using 7 baseline method to predict and compute results' AUC.
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import roc_curve, auc
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
 
 def baseline(CtrX,CtrY,CvalX,CvalY,GtrX,GtrY,GvalX,GvalY):
 
-    printer = ['LogisticRegression','KNeighborsClassifier','Linear SVC', 'rbf SVC','GaussianNB',
+    printer = ['MLPClassifier', 'LogisticRegression','KNeighborsClassifier','Linear SVC', 'rbf SVC','GaussianNB',
                'DecisionTreeClassifier','RandomForestClassifier']
-    inner = [LogisticRegression(random_state = 0),KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2),
+    inner = [ MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1), LogisticRegression(random_state = 0),KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2),
              SVC(kernel = 'linear', random_state = 0),SVC(kernel = 'rbf', random_state = 0),GaussianNB(),
              DecisionTreeClassifier(criterion = 'entropy', random_state = 0),
              RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)]
@@ -82,7 +67,19 @@ def baseline(CtrX,CtrY,CvalX,CvalY,GtrX,GtrY,GvalX,GvalY):
         plt.plot(fpr, tpr,lw=1)
         plt.text(0.5,0.3,'ROC curve (area = %0.2f)' % auc1)
         plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
-        plt.title('Genetic data with '+ printer[i] )
+        plt.title('Genetic data with ' +  printer[i])
         plt.show()
 
-baseline(Ctr_X,Ctr_Y,Cval_X,Cval_Y,Gtr_X,Gtr_Y,Gval_X,Gval_Y)
+
+if __name__ == '__main__':
+    # preprocessing data
+    preprocessor = Preprocessor()
+    clinical_X = preprocessor.clinical_X
+    clinical_Y = preprocessor.clinical_Y
+    genomic_X = preprocessor.genomic_X
+    genomic_Y = preprocessor.genomic_Y
+
+    # devide data set into 8:1:1 as train,validate,test set
+    Ctr_X, Ctr_Y, Cval_X, Cval_Y, Ct_X, Ct_Y = devide(clinical_X, clinical_Y, 10)
+    Gtr_X, Gtr_Y, Gval_X, Gval_Y, Gt_X, Gt_Y = devide(genomic_X, genomic_Y, 10)
+    baseline(Ctr_X,Ctr_Y,Cval_X,Cval_Y,Gtr_X,Gtr_Y,Gval_X,Gval_Y)
