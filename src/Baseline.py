7415a95e3b3a7387f9565aebb174218dd9fac5d9
Edward <msuhao1996@gmail.com>
2019-03-01 20:54:20 -0500

    Update Baseline.py

--- src/Baseline.py
+++ src/Baseline.py
@@ -1,22 +1,21 @@
 from preprocess import Preprocessor
 import numpy as np
 from random import randint
+import pandas as pd
 import matplotlib.pyplot as plt
 
 #preprocessing data
-
 preprocessor = Preprocessor()
-clinical_X = np.array(preprocessor.clinical_X)
-clinical_Y = np.array(preprocessor.clinical_Y)
 
-genomic_X = np.array(preprocessor.genomic_X)
-genomic_Y = np.array(preprocessor.genomic_Y)
+clinical_X = preprocessor.clinical_X
+clinical_Y = preprocessor.clinical_Y
+
+genomic_X = preprocessor.genomic_X
+genomic_Y = preprocessor.genomic_Y
 
 
 #devide data set into 8:1:1 as train,validate,test set
 
-#print(clinical_X[0])
-#print(len(clinical_X),len(genomic_X))
 def devide(X,Y,kFolds):
     devided = [[],[],[],[],[],[]]
     i=0
@@ -38,72 +37,42 @@ Ctr_X,Ctr_Y,Cval_X,Cval_Y,Ct_X,Ct_Y = devide(clinical_X,clinical_Y,10)
 Gtr_X,Gtr_Y,Gval_X,Gval_Y,Gt_X,Gt_Y = devide(genomic_X,genomic_Y,10)
 
 
-#print(len(Ctr_X),len(Ctr_Y),len(Cval_X),len(Cval_Y),len(Ct_X),len(Ct_Y))
 
 #Using 7 baseline method to predict and compute results' AUC.
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve, auc
-
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-print(fpr, tpr)
-auc1 = auc(fpr, tpr)
-print(auc1)
-plt.plot(fpr,tpr,lw=1,label='ROC fold %d (area = %0.2f)')
-plt.show()
-#Using KNeighborsClassifier Method of neighbors class to use Nearest Neighbor algorithm
+from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-auc1 = auc(fpr, tpr)
-print(auc1)
-#Using SVC method of svm class to use Support Vector Machine Algorithm
-
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-auc1 = auc(fpr, tpr)
-print(auc1)
-#Using SVC method of svm class to use Kernel SVM Algorithm
-
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-auc1 = auc(fpr, tpr)
-print(auc1)
-#Using GaussianNB method of naïve_bayes class to use Naïve Bayes Algorithm
-
+from sklearn.metrics import roc_curve, auc
 from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-auc1 = auc(fpr, tpr)
-print(auc1)
-#Using DecisionTreeClassifier of tree class to use Decision Tree Algorithm
-
 from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-auc1 = auc(fpr, tpr)
-print(auc1)
-#Using RandomForestClassifier method of ensemble class to use Random Forest Classification algorithm
-
 from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
-classifier.fit(Ctr_X, Ctr_Y)
-Y_pred = classifier.predict(Cval_X)
-fpr, tpr, thresholds = roc_curve(Cval_Y, Y_pred)
-auc1 = auc(fpr, tpr)
-print(auc1)
+
+def baseline(CtrX,CtrY,CvalX,CvalY,GtrX,GtrY,GvalX,GvalY):
+
+    printer = ['LogisticRegression','KNeighborsClassifier','Linear SVC', 'rbf SVC','GaussianNB'
+               'DecisionTreeClassifier','RandomForestClassifier']
+    inner = [LogisticRegression(random_state = 0),KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2),
+             SVC(kernel = 'linear', random_state = 0),SVC(kernel = 'rbf', random_state = 0),GaussianNB(),
+             DecisionTreeClassifier(criterion = 'entropy', random_state = 0),
+             RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)]
+    for i in range(len(printer)):
+        print('----------------------'+printer[i]+'-----------------------')
+        classifier = inner[i]
+        classifier.fit(CtrX, CtrY)
+        Y_pred = classifier.predict(CvalX)
+        fpr, tpr, thresholds = roc_curve(CvalY, Y_pred)
+        auc1 = auc(fpr, tpr)
+        print('The AUC of dealing clinical data with'+ printer[i]+'is: ',auc1)
+        plt.plot(fpr, tpr,lw=1)
+        plt.show()
+
+        classifier = inner[i]
+        classifier.fit(GtrX,GtrY)
+        Y_pred = classifier.predict(GvalX)
+        fpr, tpr, thresholds = roc_curve(GvalY, Y_pred)
+        auc2 = auc(fpr, tpr)
+        print('The AUC of dealing genetic data with'+ printer[i]+'is: ',auc2)
+        plt.plot(fpr, tpr,lw=1)
+        plt.show()
+
+baseline(Ctr_X,Ctr_Y,Cval_X,Cval_Y,Gtr_X,Gtr_Y,Gval_X,Gval_Y)
