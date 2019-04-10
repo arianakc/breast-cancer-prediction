9b9295d102971ad2b0dd3b9cac192ed15fad7cf6
Edward <msuhao1996@gmail.com>
2019-04-10 13:53:28 -0400

    Update metric.py

--- src/Xgboost_gbm/metric.py
+++ src/Xgboost_gbm/metric.py
@@ -17,23 +17,6 @@ def mean_absolute_error(preds, labels):
     return np.mean(np.abs(preds - labels))
 
 
-def tied_rank(x):
-    sorted_x = sorted(zip(x,range(len(x))))
-    r = [0 for k in x]
-    cur_val = sorted_x[0][0]
-    last_rank = 0
-    for i in range(len(sorted_x)):
-        if cur_val != sorted_x[i][0]:
-            cur_val = sorted_x[i][0]
-            for j in range(last_rank, i):
-                r[sorted_x[j][1]] = float(last_rank+1+i)/2.0
-            last_rank = i
-        if i==len(sorted_x)-1:
-            for j in range(last_rank, i+1):
-                r[sorted_x[j][1]] = float(last_rank+i+2)/2.0
-    return r
-
-
 
 metrics = {"acc": accuracy,
            "error": error,
